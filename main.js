27216ca7ca9a1c7d30b940faa83119f5aa87fb1f
vvansuit <vvansuit@outlook.com>
2024-08-15 11:57:13 -0300

    tests

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const movie = {
-    name: "Hallowen",
-    duration: 112, 
-    description: "lorem ipsum dolore etc etc",
-    ratin: 4.5, 
-    genre: ["horror", "old", "terror"],
-    casting: {
-        director: {
-            directorName: "Jhon Carpenter",
-            age: 31, 
-        },
-        actors: {
-            names: ["Actor 1","Actor 2","Actor 3","Actor 4",],
-        }
-    }
-}
+// const movie = {
+//     name: "Hallowen",
+//     duration: 112, 
+//     description: "lorem ipsum dolore etc etc",
+//     ratin: 4.5, 
+//     genre: ["horror", "old", "terror"],
+//     casting: {
+//         director: {
+//             directorName: "Jhon Carpenter",
+//             age: 31, 
+//         },
+//         actors: {
+//             names: ["Actor 1","Actor 2","Actor 3","Actor 4",],
+//         }
+//     }
+// }
 /*
 console.log(movie.genre);
 
@@ -28,27 +28,102 @@ const {name, duration, casting: {director: {directorName}} } = movie;
 console.log(directorName);
 */
 
-const todos = [
-    {
-        id: 1, 
-        string: "Take the trash",
-        isComplete: false,
-    },
-    {
-        id: 2, 
-        string: "Feed the dogos",
-        isComplete: false,
-    },
-    {
-        id: 3, 
-        string: "Take a shower",
-        isComplete: true,
-    },
-    
-];
+// :
+//Primitive Type x Reference type storage
 
-console.log(todos[1].string);
+// let firstValue = "Jhon";
 
-const todosJSON = JSON.stringify(todos);
+// let secondValue = firstValue; 
 
-console.log(todosJSON);
+// let output = {firstValue, secondValue};
+
+// console.log(output);
+
+// secondValue = "Calleb";
+
+// output = {firstValue, secondValue};
+
+// console.log(output);
+
+// let person = 
+// {
+// 	name: "Spooky",
+// 	age: 40,
+// };
+
+// newPerson = person;
+
+// let output = {person, newPerson};
+
+// console.log(output);
+
+// newPerson.name = "Houses";
+
+// output = {person, newPerson};
+
+// console.log(output);
+
+// let amount = "99.6";
+
+// console.log(amount, typeof amount);
+
+// amount = Number(amount);
+
+// amount = parseFloat(amount);
+
+// console.log(amount, typeof amount);
+
+// let amount = 100; 
+
+// amount = +amount; //convert string  to a number
+// amount = Number(amount); //convert string to a number
+// amount = parseInt(amount); //convert string to a number
+
+// console.log(amount, typeof amount);
+
+// let amount = "99.4";
+
+// amount = Number(amount);
+
+// console.log(amount, typeof amount);
+
+// amount = parseFloat(amount);
+
+// console.log(amount, typeof amount);
+
+// let amount = -123.2;
+
+// console.log(amount, typeof amount);
+
+// amount = Boolean(amount);
+
+// console.log(amount, typeof amount);
+
+// let x = 1;
+
+// x = 5;
+
+// x++;
+
+// console.log(x);
+
+// let x = 5;
+
+// x += 5;
+
+// console.log(x);
+
+// let x;
+
+// console.log(x, typeof x);
+
+// let x;
+
+// x = 5 + null;
+// console.log(x);
+
+
+
+
+
+

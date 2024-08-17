9c2eaf40cf9840350cb58613602b256411370255
vvansuit <vvansuit@outlook.com>
2024-08-17 12:10:42 -0300

    arrays challange

--- main.js
+++ main.js
@@ -517,5 +517,59 @@ console.log(rmOutput); // 31 % 15 = 1
 // x = Array.of(a, b, c);
 // console.log(x); //Will create an Array and the elements will be the content of the variables. 
 
+/**
+ * 
+ * **Instructions:**
+
+Use some of the array methods that we looked at to mutate the following array to = the expected result below:
+
+```js
+const arr = [1, 2, 3, 4, 5];
+```
+
+**Expected Result:**
+
+```js
+console.log(arr);
+// [6, 5, 4, 3, 2, 1, 0];
+ */
+// let x;
+
+// const arr = [1, 2, 3, 4, 5];
+
+// arr.push(6);
+// arr.reverse();
+// arr.push(0);
+// console.log(arr);
 
 
+/**
+ * 
+ * Challenge 2:
+
+**Instructions:**
+
+Combine `arr1` and `arr2` into a new array called `arr3` with the following elements:
+
+```js
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+````
+
+Notice that both `arr1` and `arr2` include the number 5. You will have to find a way to get rid of the extra 5.
+
+**Expected Result:**
+
+```js
+console.log(arr3);
+// [1,2,3,4,5,6,7,8,9,10]
+```
+ */
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [5, 6, 7, 8, 9, 10];
+
+// arr1.pop();
+// const arr3 = [...arr1, ...arr2];
+
+// console.log(arr3);
